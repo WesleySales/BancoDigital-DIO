5a23e23bff6947cc16ed319a1010e889b0e2116c
Wesley <wesleysales.vb@gmail.com>
2024-09-03 11:34:43 -0300

    att final

--- BancoDigital-DIO/src/BancoTestes.java
+++ BancoDigital-DIO/src/BancoTestes.java
@@ -1,26 +1,37 @@
-import entities.Cliente;
-import entities.Conta;
+
+import repositories.ClienteRepository;
+import repositories.ContaRepository;
 import services.ClienteService;
 import services.ContaService;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class BancoTestes {
 
     public static void main(String[] args) {
 
-        ContaService contaService = new ContaService();
+        ClienteRepository clienteRepository = new ClienteRepository();
+        ContaRepository contaRepository = new ContaRepository();
+
         ClienteService clienteService = new ClienteService();
+        ContaService contaService = new ContaService(contaRepository, clienteService);
 
         clienteService.cadastrarCliente("Wesley Sales","wesley@gmail.com","1");
         clienteService.cadastrarCliente("Joao Mercio","joao@gmail.com","2");
         clienteService.cadastrarCliente("Maria Madalena","maria@gmail.com","3");
 
-        System.out.println(clienteService.findByCPF("1"));
-        contaService.criarConta("1");
+//        System.out.println(clienteService.findByCPF("1"));
+
+        System.out.println(clienteService.findAll());
+
+        contaService.criarConta(100l);
+        contaService.criarConta(101l);
+        contaService.criarConta(102l);
+        System.out.println(contaService.findAllConta());
+//        Cliente cliente1 = clienteService.getById(100l);
+//        Conta conta = new Conta(cliente1);
+//        System.out.println(conta);
 //        System.out.println();
-        System.out.println(clienteService.findAllCliente());
+//        System.out.println(b);
 
 //        contaService. depositar(conta1,1000);
 //        contaService. depositar(conta2,900);
